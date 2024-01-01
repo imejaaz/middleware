9378c7b30de7672d242030a01aacbe849d5a1173
Ijaz Ahmad <Ijaz.professional@gmail.com>
2024-01-01 10:30:42 +0500

    add session storage

--- session/views.py
+++ session/views.py
@@ -4,18 +4,36 @@ from django.shortcuts import render
 # Create your views here.
 def setSession(request):
 
+    request.session.set_expiry(90)
     request.session['name'] = 'ejaz ahmad'
+    request.session['age'] = '24'
     obj = render(request, 'session/set.html')
     return obj
 
 
 def getSession(request):
 
+    # name = request.session.get('name', '')
+    # age = request.session.get('age', '')
+    # items = request.session.items()
+    # keys = request.session.keys()
+    # request.session.setdefault('age', 20)
+    # return render(request, 'session/get.html', {'name' :name, 'age':age, 'keys': keys, 'items':items})
+
     name = request.session.get('name', '')
+    # print(request.session.get_session_cookie_age)
+    # print(request.session.get_expiry_date)
+    # print(request.session.get_expiry_age)
+    # print(request.session.get_expire_at_browser_close)
+
     return render(request, 'session/get.html', {'name' :name})
 
 def deleteSession(request):
-    if 'name' in request.session:
-        del request.session['name']
+
+    # if 'name' in request.session:
+    #     del request.session['name']
+
+    request.session.clear_expired()
+    request.session.flush()
 
     return render(request, 'session/del.html')
